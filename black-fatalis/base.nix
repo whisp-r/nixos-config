9a9edf152725b882d974546f4e88517158b28ab5
whisp-r <188515789+whisp-r@users.noreply.github.com>
2026-01-21 16:33:43 +0100

    nix-ld enabled for black fatalis

--- black-fatalis/base.nix
+++ black-fatalis/base.nix
@@ -20,6 +20,17 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # Allow NixOS to run unpatched executables
+  programs.nix-ld = {
+    enable = true;
+    libraries = [
+      ## Uncomment if you want to use the libraries provided by default in the steam distribution
+      ## but this is quite far from being exhaustive
+      ## https://github.com/NixOS/nixpkgs/issues/354513
+      # (pkgs.runCommand "steamrun-lib" {} "mkdir $out; ln -s ${pkgs.steam-run.fhsenv}/usr/lib64 $out/lib")
+    ];
+  }
+
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
