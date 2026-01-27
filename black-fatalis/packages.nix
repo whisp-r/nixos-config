c969537055eb771129fea77c0d7ea49933619e0e
whisp-r <188515789+whisp-r@users.noreply.github.com>
2026-01-27 17:16:57 +0100

    konsave

--- black-fatalis/packages.nix
+++ black-fatalis/packages.nix
@@ -19,7 +19,15 @@ in
 
       jetbrains.clion
     ]
-    ++ [ # custom packages
+    ++ [
+      # custom packages
       edu-sync-cli
     ];
+
+  # programs.steam = {
+  #   enable = true;
+  #   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  #   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  #   localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  # };
 }
