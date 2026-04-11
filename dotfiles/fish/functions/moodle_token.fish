02f798802e896396aefdcb45a29239bafc781449
whisp-r <188515789+whisp-r@users.noreply.github.com>
2026-04-11 12:51:52 +0200

    update script clarification

--- dotfiles/fish/functions/moodle_token.fish
+++ dotfiles/fish/functions/moodle_token.fish
@@ -1,6 +1,7 @@
 function moodle_token --description "Takes domain and password and curls back a token for a moodle instance" -a domain user pass
-    echo "running script with: $domain, $user $pass"
+    echo "running script with: domain:$domain, user:$user pass:$pass"
     set url_pass (string escape --style=url $pass)
+    echo "domain: $domain"
     echo "url_pass: $domain, $user, $pass"
     set url "https://$domain/login/token.php?username=$user&password=$url_pass&service=moodle_mobile_app"
     echo $url
