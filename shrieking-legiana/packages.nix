c00a63622145314a2745487d75d170a8116976b6
whisp-r <188515789+whisp-r@users.noreply.github.com>
2026-04-11 12:52:15 +0200

    edu-sync sl

--- shrieking-legiana/packages.nix
+++ shrieking-legiana/packages.nix
@@ -1,21 +1,24 @@
-{ pkgs, ... }:
+{ pkgs, inputs, system, ... }:
 
+let
+  edu-sync-cli = inputs.edu-sync-nix.packages.${system}.default;
+in
 {
   # services.xserver.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    # none
-    # imhex
-    vesktop
-    # firefox-devedition
-    # krita
-    # jetbrains.webstorm
-    # docker
+  environment.systemPackages =
+    with pkgs;
+    [
+      vesktop
+      zig
+      zls
+      onlyoffice-desktopeditors
+    ]
 
-    # ghidra
-    # wireshark
-    # obsidian
-  ];
+    ++ [
+      # custom packages
+      edu-sync-cli
+    ];
 
   programs = {
     thunderbird.enable = true;
