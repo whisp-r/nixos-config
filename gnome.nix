9d2609d86eb8a9fe04a8bccd9caf1fa0cb6d3909
whisp-r <188515789+whisp-r@users.noreply.github.com>
2026-04-12 14:13:47 +0200

    app indicator gnome

--- gnome.nix
+++ gnome.nix
@@ -9,11 +9,12 @@
   services.gnome.games.enable = false;
 
   environment.gnome.excludePackages = with pkgs; [
-    gnome-tour
+    # gnome-tour
     # gnome-user-docs
   ];
 
   environment.systemPackages = with pkgs; [
     gnomeExtensions.clipboard-history
+    gnomeExtensions.appindicator
   ];
 }
