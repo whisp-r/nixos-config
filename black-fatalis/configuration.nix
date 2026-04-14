7ede65cb35c717d8b31390a61856363b331ebd47
whisp-r <188515789+whisp-r@users.noreply.github.com>
2026-04-14 14:25:21 +0200

    configuration tidyied up

--- black-fatalis/configuration.nix
+++ black-fatalis/configuration.nix
@@ -1,3 +1,7 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
 { pkgs, ... }:
 
 {
@@ -5,16 +9,31 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Configure network connections interactively with nmcli or nmtui.
+  networking.networkmanager.enable = true;
+
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   boot.initrd.luks.devices."luks-53e7c03d-d40e-43ff-9c19-a12301792bfd".device =
     "/dev/disk/by-uuid/53e7c03d-d40e-43ff-9c19-a12301792bfd";
-  networking.hostName = "black-fatalis"; # Define your hostname.
 
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true; # Easiest and most distros use
-
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.05"; # Did you read the comment?
 }
