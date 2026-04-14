b30efebd22913fed7a10b488d3c663fd6b7e579f
whisp-r <188515789+whisp-r@users.noreply.github.com>
2026-04-14 20:04:48 +0200

    plymouth update

--- plymouth.nix
+++ plymouth.nix
@@ -1,31 +1,41 @@
 { pkgs, ... }:
 
+# https://discourse.nixos.org/t/how-to-configure-a-graphical-boot-screen-with-luks-unlock/63357
 {
   boot = {
 
-    plymouth = {
-      enable = true;
-      # theme = "rings";
-      # themePackages = with pkgs; [
-      #   # By default we would install all themes
-      #   (adi1090x-plymouth-themes.override {
-      #     selected_themes = [ "rings" ];
-      #   })
-      # ];
-    };
+    # plymouth = {
+    #   enable = true;
+    #   # theme = "rings";
+    #   # themePackages = with pkgs; [
+    #   #   # By default we would install all themes
+    #   #   (adi1090x-plymouth-themes.override {
+    #   #     selected_themes = [ "rings" ];
+    #   #   })
+    #   # ];
+    # };
 
-    # Enable "Silent boot"
+    # plymouth, showing after LUKS unlock
+    plymouth.enable = true;
+    # plymouth.font = "${pkgs.hack-font}/share/fonts/truetype/Hack-Regular.ttf";
+    # plymouth.logo = "${pkgs.nixos-icons}/share/icons/hicolor/128x128/apps/nix-snowflake.png";
+
+    # silence first boot output
     consoleLogLevel = 3;
     initrd.verbose = false;
+    initrd.systemd.enable = true;
     kernelParams = [
-      "quiet"
-      "udev.log_level=3"
-      "systemd.show_status=auto"
+        "quiet"
+        "splash"
+        "intremap=on"
+        "boot.shell_on_fail"
+        "udev.log_priority=3"
+        "rd.systemd.show_status=auto"
     ];
 
     # Hide the OS choice for bootloaders.
     # It's still possible to open the bootloader list by pressing any key
     # It will just not appear on screen unless a key is pressed
-    loader.timeout = 0;
+    loader.timeout = 1;
   };
 }
