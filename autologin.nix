5926a92084e08ecc30f5af761a1e3949964ea0b2
whisp-r <188515789+whisp-r@users.noreply.github.com>
2026-05-29 21:06:43 +0200

    auto login nicer

--- autologin.nix
+++ autologin.nix
@@ -1,6 +1,8 @@
 { username, ... }:
 
 {
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = username;
+  services.displayManager.autoLogin = {
+    enable = true;
+    user = username;
+  };
 }
